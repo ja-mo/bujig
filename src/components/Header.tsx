fad0f8583e625275744eb074f4df20b9aba6ef4d
mohammad <dear.jamo@yahoo.com>
2024-12-26 08:31:04 +0330

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,38 @@
-/*haader comming soon*/
+import React from 'react';
+import { IonButton, IonContent, IonHeader, IonMenu, IonMenuToggle, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+
+const Header: React.FC = () => {
+
+  return (
+<>
+      <IonMenu contentId="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu Content</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonMenuToggle>
+            <IonButton>Click to close the menu</IonButton>
+          </IonMenuToggle>
+        </IonContent>
+
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonMenuToggle>
+            <IonButton>Click to open the menu</IonButton>
+          </IonMenuToggle>
+        </IonContent>
+      </IonPage>
+      </IonMenu>
+    </>
+  );
+};
+
+export default Header;
